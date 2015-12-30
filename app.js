4d16e0cb91043cd567203c682a6ba1d50b2f07cc
Karol Maj <karol.maj@schibsted.pl>
2015-12-30 11:04:10 +0100

    Added CORS support

--- app.js
+++ app.js
@@ -17,8 +17,13 @@ module.exports = function (repository) {
     app.use(bodyParser.json());
 
     app.use(function(req, res, next) {
+        //res.header("Access-Control-Allow-Origin", "*");
+        //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+        res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+
         next();
     });
 
